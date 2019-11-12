36055c445b1133318481eb226d1f9a4e3bc3d62c
Izabela Kuzniar <iza8848@gmail.com>
2019-11-12 14:32:40 +0100

    added model classes: Person, PersonBook, Review

--- BooksTry/Models/PersonBook.cs
+++ BooksTry/Models/PersonBook.cs
@@ -7,5 +7,20 @@ namespace BooksTry.Models
 {
     public class PersonBook
     {
+        public int PBId { get; set; }
+        public int PersonId { get; set; }
+        public int BookId { get; set; }
+
+        public PersonBook()
+        {
+
+        }
+
+        public PersonBook(int pBId, int personId, int bookId)
+        {
+            PBId = pBId;
+            PersonId = personId;
+            BookId = bookId;
+        }
     }
 }
