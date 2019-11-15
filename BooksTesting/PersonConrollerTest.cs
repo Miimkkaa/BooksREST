6548e750bfa00ce3f97a14a0645a7e6857c3fe77
Izabela Kuzniar <iza8848@gmail.com>
2019-11-15 12:45:51 +0100

    Added: Unit testing for Person Controller

---
+++ BooksTesting/PersonConrollerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BooksTry.Controllers;
+using BooksTry.Models;
+using System.Linq;
+
+namespace BooksTesting
+{
+    [TestClass]
+    public class PersonConrollerTest
+    {
+        private readonly PersonController _controller = new PersonController();
+
+        [TestMethod]
+        public void TestGetAll()
+        {
+            IEnumerable<Person> persons = _controller.Get();
+            Assert.AreEqual(3, persons.Count());
+
+            Person person = _controller.Get(1); //get by id
+            Assert.AreEqual("mimka", person.Username);
+        }
+    }
+}
