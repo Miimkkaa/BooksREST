01c38808ac29f13865b3a1db1ba48c530d35fba9
Miimkkaa <33956333+Miimkkaa@users.noreply.github.com>
2019-11-15 12:23:44 +0100

    Unit testing for BookController, only getAll and get by id

--- BooksTesting/UnitTest1.cs
+++ BooksTesting/UnitTest1.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using BooksTry.Controllers;
+using BooksTry.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BooksTesting
@@ -5,9 +9,16 @@ namespace BooksTesting
     [TestClass]
     public class UnitTest1
     {
+        private readonly BookController _controller = new BookController();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestGetAll()
         {
+            IEnumerable<Book> books = _controller.Get();
+            Assert.AreEqual(24, books.Count());
+
+            Book book = _controller.Get(8); //Get by id
+            Assert.AreEqual("Moby Dick", book.Title);
         }
     }
 }
